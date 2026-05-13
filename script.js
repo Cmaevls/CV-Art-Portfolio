52e973cecb1d1b86ac0d31535cbdf5281266c15b
Cmaevls <christineaviles1010@gmail.com>
2026-05-13 23:25:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const slides = document.querySelectorAll(".slide");
-const buttons = document.querySelectorAll(".view-btn");
+const buttons = document.querySelectorAll(".next-btn");
 
 let current = 0;
 
-buttons.forEach((btn, index) => {
+buttons.forEach(button => {
 
-  btn.addEventListener("click", () => {
+  button.addEventListener("click", () => {
 
     slides[current].classList.remove("active");
+    slides[current].classList.add("prev");
 
-    current = (current + 1) % slides.length;
+    current++;
+
+    if(current >= slides.length){
+      current = 0;
+    }
+
+    slides.forEach(slide => {
+
+      if(!slide.classList.contains("active")){
+        slide.classList.remove("prev");
+      }
+
+    });
 
     slides[current].classList.add("active");
 
